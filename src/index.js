0c3a8ed318151194b72dcb25edc5bdf82db1fc11
lekegitrepo <lekedevrepo@gmail.com>
2020-03-07 14:37:37 -0600

    add bomb to the game scene

--- src/index.js
+++ src/index.js
@@ -18,9 +18,10 @@ const config = {
     }
 };
 
+let bombs
+let cursors;
 let player;
 let platforms;
-let cursors;
 let score = 0;
 let scoreText;
 
@@ -83,14 +84,18 @@ function create ()
     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
   });
 
+  bombs = this.physics.add.group();
+
   cursors = this.input.keyboard.createCursorKeys();
 
   scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
 
   this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update ()
@@ -125,4 +130,31 @@ function collectStar (player, star)
   star.disableBody(true, true);
   score += 10;
   scoreText.setText('Score: ' + score);
+
+  if (stars.countActive(true) === 0)
+  {
+    //  A new batch of stars to collect
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+    const bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+}
+
+function hitBomb (player, bomb)
+{
+  this.physics.pause();
+
+  player.setTint(0xff0000);
+
+  player.anims.play('turn');
+
+  gameOver = true;
 }
